1244d46fe9ba97e8e4b08874d214a9905e5f13a7
ash <mutemallow@gmail.com>
2026-02-06 22:28:36 +0700

    Add unit base classes

---
+++ src/main/java/com/Expedition67/unit/UnitBrain.java
@@ -0,0 +1,47 @@
+package com.Expedition67.unit;
+
+public abstract class UnitBrain {
+    //use to manage the logic of unit, both player and enemies
+    
+    protected Unit owner;
+
+    public final void setOwner(Unit owner) {
+        this.owner = owner;
+    }
+
+    public void update(){
+
+    }
+    
+    public abstract void takeDamage(Unit src, float amount);
+    public abstract void startTurn();
+    public abstract void endTurn();
+
+    public void heal(float amount){
+
+    }
+
+    public void addDef(float amount, int forTurn){
+
+    }
+
+    public void addAgi(float amount, int forTurn){
+
+    }
+
+    public void addCritRate(float amount, int forTurn){
+
+    }
+
+    public void addCritDmg(float amount, int forTurn){
+
+    }
+
+    private void onTurnStarted(){
+
+    }
+
+    private void onTurnEnded(){
+
+    }
+}
