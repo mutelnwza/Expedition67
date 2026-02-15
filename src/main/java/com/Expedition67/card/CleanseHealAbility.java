c97bb3842daadad55d560c6343c5819e2f517ab7
chasrimeku <chayada.p21@gmail.com>
2026-02-16 01:50:28 +0700

    add skeleton ability classes for 7 remaining cards
    - Added AoEDamageAbility (Echoing Strike)
    - Added VoidDragonAbility (Void Dragon)
    - Added SingularityAbility (Celestial Singularity)
    - Added SoulResonanceAbility (Soul Resonance)
    - Added HarmonicResonanceAbility (Harmonic Resonance)
    - Added CleanseHealAbility (Ethereal Restoration)
    - Added RebirthAbility (Eternal Soul Rebirth)
    
    Note: Implemented basic logic and added // TODO comments waiting for core system integration (MaxHP, AoE, Hand management, Cleanse).

---
+++ src/main/java/com/Expedition67/card/CleanseHealAbility.java
@@ -0,0 +1,16 @@
+package com.Expedition67.card;
+import com.Expedition67.unit.Unit;
+
+public class CleanseHealAbility implements CardAbility {
+    private int healAmount;
+
+    public CleanseHealAbility(int healAmount) {
+        this.healAmount = healAmount;
+    }
+
+    @Override
+    public void apply(Unit target) {
+        target.getBrain().heal(healAmount);
+        // TODO: รอทำระบบ ลบล้างสถานะผิดปกติ (Remove all debuffs)
+    }
+}
---
+++ src/main/java/com/Expedition67/card/SoulResonanceAbility.java
@@ -0,0 +1,15 @@
+package com.Expedition67.card;
+import com.Expedition67.unit.Unit;
+
+public class SoulResonanceAbility implements CardAbility {
+    private int damageBuff;
+
+    public SoulResonanceAbility(int damageBuff) {
+        this.damageBuff = damageBuff;
+    }
+
+    @Override
+    public void apply(Unit target) {
+        // TODO: รอทำระบบ บวกดาเมจให้การ์ด Attack ทุกใบในมือ
+    }
+}
